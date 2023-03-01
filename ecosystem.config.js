ea061dbff1accfb66f75015b2639520b8c918b99
Milton Elizalde <alexmol_14@outlook.com>
2023-03-01 16:14:00 -0500

    cambios al ecosystem.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,6 +11,11 @@ module.exports = {
     }],
     deploy: {
       production: {
+      user: 'root',
+      host: '127.0.0.1',
+      ref: 'origin/main',
+      repo: 'https://github.com/boomcrash/Aula-Estudiantil-Back-End.git',
+      path: '/home/aula-backend',
       // Configuración de Git
       "post-deploy": "git pull origin main && pip install -r requirements.txt && pm2 reload ecosystem.config.js --env production"
       }
