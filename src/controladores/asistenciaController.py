fad3d7ad187b056a0bc791f40a9620c8752e1504
Milton Elizalde <alexmol_14@outlook.com>
2023-03-02 13:38:39 -0500

    nuevos servicios en asistencia

--- src/controladores/asistenciaController.py
+++ src/controladores/asistenciaController.py
@@ -9,7 +9,7 @@ from pydantic import BaseModel
 # parametros de peticiones http en body
 from fastapi.param_functions import Body
 #importacion de clases de usuario
-from clases.asistenciaClass import getAsistenciaEstudianteId
+from clases.asistenciaClass import getAsistenciaEstudianteId,getAsistenciaEstudianteByFechaAndCurso,getAsistenciaEstudianteByCursoAndEstudiante
 
 asistencia_router = APIRouter()
 
@@ -56,4 +56,55 @@ async def getAsistenciasByIdEstudiante(request: Request, asistencia: getAsistenc
         conn.close()
 
 
+#post para obtener asistencias by fecha con esta sentencia
+#SELECT id_estudiante, 
+#		concat(nombres_estudiante, ' ', apellidos_estudiante) as nombrescompletos_estudiante, 
+#		estado_asistencia
+#FROM Asistencia, Estudiante
+#WHERE fecha_asistencia = '2022-11-03'
+#		AND curso_asistencia = 1
+#		AND estudiante_asistencia = id_estudiante;
+@asistencia_router.post("/getAsistenciasByFechaAndCurso")
+async def getAsistenciasByFechaAndCurso(request: Request, asistencia: getAsistenciaEstudianteByFechaAndCurso = Body(...)):
+    conn = await getConexion()
+    try:
+        asistencias=[]
+        async with conn.cursor() as cur:
+            await cur.execute("SELECT id_estudiante, concat(nombres_estudiante, ' ', apellidos_estudiante) as nombrescompletos_estudiante, estado_asistencia FROM Asistencia, Estudiante WHERE fecha_asistencia = %s AND curso_asistencia = %s AND estudiante_asistencia = id_estudiante", (asistencia.fecha_asistencia,asistencia.curso_asistencia))
+            resultado = await cur.fetchall()
+            for result in resultado:
+                asistencia = {'id_estudiante': result['id_estudiante'],'nombrescompletos_estudiante': result['nombrescompletos_estudiante'],'estado_asistencia': result['estado_asistencia']}
+                asistencias.append(asistencia)
+        return {'data': asistencias, 'accion': True}
+    
+    except Exception as e:
+        return {'data': '', 'accion': False}
+    finally:
+        conn.close()
+
+
+
+#post para obtener asistencias by curso con esta sentencia SELECT  fecha_asistencia, 
+#		estado_asistencia
+#FROM Asistencia, Estudiante
+#WHERE  curso_asistencia = 1
+#	   AND estudiante_asistencia = id_estudiante
+#       AND estudiante_asistencia = 1;
+@asistencia_router.post("/getAsistenciasByCursoAndEstudiante")
+async def getAsistenciasByCursoAndEstudiante(request: Request, asistencia: getAsistenciaEstudianteByCursoAndEstudiante = Body(...)):
+    conn = await getConexion()
+    try:
+        asistencias=[]
+        async with conn.cursor() as cur:
+            await cur.execute("SELECT  fecha_asistencia, estado_asistencia FROM Asistencia, Estudiante WHERE  curso_asistencia = %s AND estudiante_asistencia = id_estudiante AND estudiante_asistencia = %s", (asistencia.curso_asistencia,asistencia.estudiante_asistencia))
+            resultado = await cur.fetchall()
+            for result in resultado:
+                asistencia = {'fecha_asistencia': result['fecha_asistencia'],'estado_asistencia': result['estado_asistencia']}
+                asistencias.append(asistencia)
+        return {'data': asistencias, 'accion': True}
+    
+    except Exception as e:
+        return {'data': '', 'accion': False}
+    finally:
+        conn.close()
 
