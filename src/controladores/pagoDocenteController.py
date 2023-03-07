d4f7a63ad9caa6f5cc17b23f145b22b454390d88
boomcrash <alexmol_14@outlook.com>
2023-03-07 11:55:02 -0500

    get admin pagoDocentes

--- src/controladores/pagoDocenteController.py
+++ src/controladores/pagoDocenteController.py
@@ -38,7 +38,7 @@ async def getPagoDocentes():
 
 #post de pago docente by docente_pagoDocente
 @pagoDocente_router.post("/getPagoDocenteByDocenteId")
-async def getPagoDocenteByDocenteId(pagoDocente: getPagoDocenteByDocenteId):
+async def getPagoDocenteByDocenteId(request: Request, pagoDocente: getPagoDocenteByDocenteId):
     conn = await getConexion()
     try:
         pagoDocentes=[]
@@ -47,6 +47,35 @@ async def getPagoDocenteByDocenteId(pagoDocente: getPagoDocenteByDocenteId):
             resultado = await cur.fetchall()
             for result in resultado:
                 pagoDocente = {'id_pagoDocente': result['id_pagoDocente'],'docente_pagoDocente': result['docente_pagoDocente'],'fecha_pagoDocente': result['fecha_pagoDocente'],'faltas_pagoDocente': result['faltas_pagoDocente'],'descuento_pagoDocente': result['descuento_pagoDocente'],'total_pagoDocente': result['total_pagoDocente']}
+                pagoDocentes.append(pagoDocente)
+        return {'data': pagoDocentes, 'accion': True}
+    
+    except Exception as e:
+        return {'data': '', 'accion': False}
+    finally:
+        conn.close()
+
+
+@pagoDocente_router.get("/getAdminPagoDocentes")
+async def getAdminPagoDocentes():
+    conn = await getConexion()
+    try:
+        pagoDocentes=[]
+        async with conn.cursor() as cur:
+            await cur.execute("""SELECT fecha_pagoDocente, sueldo_contrato, faltas_pagoDocente, 
+                                descuento_pagoDocente, total_pagoDocente, cedula_docente, concat(nombres_docente, ' 
+                                ', apellidos_docente) AS nombresCompletos_docente
+                                FROM Docente, PagoDocente, Contrato
+                                WHERE docente_pagoDocente = id_docente = docente_contrato;""")
+            resultado = await cur.fetchall()
+            for result in resultado:
+                pagoDocente = {'fecha_pagoDocente': result['fecha_pagoDocente'],
+                               'sueldo_contrato': result['sueldo_contrato'],
+                               'faltas_pagoDocente': result['faltas_pagoDocente'],
+                               'descuento_pagoDocente': result['descuento_pagoDocente'],
+                               'total_pagoDocente': result['total_pagoDocente'],
+                               'cedula_docente': result['cedula_docente'],
+                               'nombresCompletos_docente': result['nombresCompletos_docente']}
                 pagoDocentes.append(pagoDocente)
         return {'data': pagoDocentes, 'accion': True}
     
